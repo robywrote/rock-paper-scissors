d73dcff56bb10d45553de9af23afa70f7e91b22f
Sean Roby <robywrote@gmail.com>
2022-03-24 06:47:26 -0500

    add playRound

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
+'use strict'
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-const possible = ["Rock", "Paper", "Scissors"];
+const possible = ["rock", "paper", "scissors"];
 
 function computerPlay() {
     return possible[getRandomInt(3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return 'draw'
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    return 'Rock smashes scissors! You win!'
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return 'Paper covers rock! You lose!'
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return 'Paper covers rock! You win!'
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    return 'Scissors cut paper! You lose!'
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    return 'Scissors cut paper! You win!'
+  } else {
+    // player: scissors, computer: rock
+    return 'Rock smashes scissors! You lose!'
+  }
 }
